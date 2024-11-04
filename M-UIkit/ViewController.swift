be4e23399782bd2b77cc5bc493dd3fa4f9ff7243
Roman Khovalko <hromnik@me.com>
2024-11-04 11:11:54 +0200

    Initial Commit

---
+++ M-UIkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M-UIkit
+//
+//  Created by iHRomnik on 04.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ M-UIkitTests/M_UIkitTests.swift
@@ -0,0 +1,17 @@
+//
+//  M_UIkitTests.swift
+//  M-UIkitTests
+//
+//  Created by iHRomnik on 04.11.2024.
+//
+
+import Testing
+@testable import M_UIkit
+
+struct M_UIkitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
